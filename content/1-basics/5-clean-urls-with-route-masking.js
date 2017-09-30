eac3335e488a880e2a1c396cf7f978ae4cd88b78
Ernest Okot <okot08@gmail.com>
2017-09-30 03:27:58 +0300

    Corrected typo

--- content/1-basics/5-clean-urls-with-route-masking.js
+++ content/1-basics/5-clean-urls-with-route-masking.js
@@ -68,7 +68,7 @@ import Link from 'next/link'
 
 const PostLink = (props) => (
   <li>
-    <Link as={\`/p/\${props.id}\`} href={\`/post?title=\${props.title}\`}>
+    <Link as={\`/p/\${props.title}\`} href={\`/post?title=\${props.title}\`}>
       <a>{props.title}</a>
     </Link>
   </li>
@@ -91,7 +91,7 @@ Have look at the following code block:
 ~~~js
 const PostLink = (props) => (
   <li>
-    <Link as={\`/p/\${props.id}\`} href={\`/post?title=\${props.title}\`}>
+    <Link as={\`/p/\${props.title}\`} href={\`/post?title=\${props.title}\`}>
       <a>{props.title}</a>
     </Link>
   </li>
